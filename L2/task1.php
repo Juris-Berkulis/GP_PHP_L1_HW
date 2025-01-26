26f3d89d149782a7d4c78c406efc181cc8b94f4e
Juris-Berkulis <krztfjrbhm1@gmail.com>
2025-01-26 21:07:02 +0300

    refactoring (L2, task1): Change solution.

--- L2/task1.php
+++ L2/task1.php
@@ -1,21 +1,16 @@
 <?php
 do {
-    $isSuccess = true;
     $answer = readline("В каком году произошло крещение Руси?\nВарианты: 810, 988 или 740 год");
 
     switch ($answer) {
         case "810":
         case "740": {
             echo "Ответ неверный!";
-            break;
+            break(2);
         }
         case "988": {
             echo "Поздравляем с правильным ответом!";
-            break;
-        }
-        default: {
-            $isSuccess = false;
-            break;
+            break(2);
         }
     }
-} while (!$isSuccess);
+} while (true);
