413b31ffe29de8938fda3a810546187e260950f0
Juris-Berkulis <krztfjrbhm1@gmail.com>
2025-05-25 00:58:06 +0300

    feat (L6): Display the tasks tab in the menu only for authorized users.

--- L6/view/menu.php
+++ L6/view/menu.php
@@ -1,10 +1,10 @@
 <a href="/">Главная</a>
 <?php if ($username === null) : ?>
-    <a href="/?controller=security">Авторизации</a>
+    <a href="/?controller=security">Авторизация</a>
 <?php else : ?>
+    <a href="/?controller=tasks">Задачи</a>
     <a href="/?controller=security&action=logout">Выход</a>
 <?php endif;?>
-<a href="/?controller=tasks">Задачи</a>
 <br>
 
 <?php if ($username !== null) : ?>
