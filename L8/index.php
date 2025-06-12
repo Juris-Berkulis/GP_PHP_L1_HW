d758add09c1d875cd96014b9321c470b9a891426
Juris-Berkulis <krztfjrbhm1@gmail.com>
2025-06-12 23:39:51 +0300

    update (L8): Add error handling.

--- L8/index.php
+++ L8/index.php
@@ -4,8 +4,22 @@
 // 1. В консоли запустить сервер командой "php8.3 -S localhost:80 L8/index.php";
 // 2. В браузере перейти по адресу "http://localhost/".
 
-$controller = $_GET['controller'] ?? 'index';
+try {
+    $controller = $_GET['controller'] ?? 'index';
 
-$routes = require 'routes.php';
+    $routes = require 'routes.php';
 
-require_once $routes[$controller] ?? die('404');
+    require_once $routes[$controller] ?? die('404');
+} catch (PDOException $exception) {
+    echo 'Проблемы с базой данных!';
+    var_dump($exception);
+} catch (Exception $exception) {
+    echo 'Неизвестное исключение!';
+    var_dump($exception);
+} catch (Error $error) {
+    echo 'Неизвестная ошибка!';
+    var_dump($error);
+} catch (Throwable $throwable) {
+    echo 'Неизвестное исключение или ошибка!';
+    var_dump($throwable);
+}
